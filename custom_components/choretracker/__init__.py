cc55255e175d8588feb56a37eae926e7bb1e85c7
nyitraijoco <nyitrai.joco@gmail.com>
2024-01-09 14:36:20 +0000

    move choresCollection to chores_entity.py

--- custom_components/choretracker/__init__.py
+++ custom_components/choretracker/__init__.py
@@ -1,6 +1,6 @@
 import logging
 import uuid
-from homeassistant.helpers.entity import Entity
+from .chores_entity import ChoresCollection
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -25,33 +25,3 @@ async def async_add_chore(self, chore_data):
     chore_data['id'] = chore_id  # Assign the generated ID to the chore
     chore_data['effort'] = effort  # Update the chore's effort
     self._chores[chore_id] = chore_data
-
-class ChoresCollection(Entity):
-    """Representation of a Chores Collection."""
-
-    def __init__(self):
-        """Initialize the Chores Collection."""
-
-    @property
-    def name(self):
-        """Return the name of the entity."""
-        return 'Chores Collection'
-
-    @property
-    def state(self):
-        """Return the state of the entity."""
-        # For a group entity, the state might not be meaningful
-        return None
-
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes of the entity."""
-        return self._chores
-
-    async def async_update(self, chore_id=None):
-        """Update the entity or specific chore's last done date."""
-        if chore_id:
-            if chore_id in self._chores:
-                self._chores[chore_id]['last_done'] = datetime.now().strftime('%Y-%m-%d')
-            else:
-                _LOGGER.warning(f"Chore with ID '{chore_id}' not found.")
