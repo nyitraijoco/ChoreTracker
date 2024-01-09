32b9e57cc0af7655064b4c4d1f4c3a88e8c43a0c
nyitraijoco <nyitrai.joco@gmail.com>
2024-01-09 14:13:45 +0000

    Add async_setup_entry and put ChoresCollection in separated file.

---
+++ chores_entity.py
@@ -0,0 +1,36 @@
+import logging
+import uuid
+from homeassistant.const import STATE_UNKNOWN
+from homeassistant.helpers.entity import Entity
+
+_LOGGER = logging.getLogger(__name__)
+
+class ChoresCollection(Entity):
+    """Representation of a Chores Collection."""
+
+    def __init__(self):
+        """Initialize the Chores Collection."""
+
+    @property
+    def name(self):
+        """Return the name of the entity."""
+        return 'Chores Collection'
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        # For a group entity, the state might not be meaningful
+        return None
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes of the entity."""
+        return self._chores
+
+    async def async_update(self, chore_id=None):
+        """Update the entity or specific chore's last done date."""
+        if chore_id:
+            if chore_id in self._chores:
+                self._chores[chore_id]['last_done'] = datetime.now().strftime('%Y-%m-%d')
+            else:
+                _LOGGER.warning(f"Chore with ID '{chore_id}' not found.")
--- custom_components/choretracker/__init__.py
+++ custom_components/choretracker/__init__.py
@@ -1,46 +1,21 @@
 import logging
 import uuid
 from homeassistant.helpers.entity import Entity
+from chores_entity import ChoresCollection
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'chore_tracker'
 
+async def async_setup_entry(hass, config_entry):
+    """Set up the Chores Collection entity."""
+    hass.data.setdefault('chores_collection', ChoresCollection())
+    return True
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the Custom Entity."""
     async_add_entities([ChoresCollection()])
 
-
-class ChoresCollection(Entity):
-    """Representation of a Chores Collection."""
-
-    def __init__(self):
-        """Initialize the Chores Collection."""
-
-    @property
-    def name(self):
-        """Return the name of the entity."""
-        return 'Chores Collection'
-
-    @property
-    def state(self):
-        """Return the state of the entity."""
-        # For a group entity, the state might not be meaningful
-        return None
-
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes of the entity."""
-        return self._chores
-
-    async def async_update(self, chore_id=None):
-        """Update the entity or specific chore's last done date."""
-        if chore_id:
-            if chore_id in self._chores:
-                self._chores[chore_id]['last_done'] = datetime.now().strftime('%Y-%m-%d')
-            else:
-                _LOGGER.warning(f"Chore with ID '{chore_id}' not found.")
 
 async def async_add_chore(self, chore_data):
     """Add a new chore."""
